fc2d82f797fab142751d4adb7d0c3b968c4438fe
dann9109 <danielxstella@gmail.com>
2024-02-06 09:55:23 -0500

    seed fix

--- controllers/form_controller.js
+++ controllers/form_controller.js
@@ -1,25 +1,25 @@
-const User =require('../models/User')
+const User = require('../models/User')
 
 module.exports = {
-async createUser(req, res) {
+  async createUser(req, res) {
     try {
-        const user = await User.create(req.body);
-    
-        req.session.user_id = user.id;
-    
-        res.redirect('/');
-      } catch (err) {
-        const messages = err.errors.map(eObj => eObj.message);
-    
-        req.session.errors = messages;
-    
-        console.log(err);
-    
-        res.redirect('/register');
-      }
-},
+      const user = await User.create(req.body);
 
-async loginUser(req,res) {
+      req.session.user_id = user.id;
+
+      res.redirect('/');
+    } catch (err) {
+      const messages = err.errors.map(eObj => eObj.message);
+
+      req.session.errors = messages;
+
+      console.log(err);
+
+      res.redirect('/register');
+    }
+  },
+
+  async loginUser(req, res) {
     const { email, password } = req.body;
 
     try {
@@ -28,24 +28,24 @@ async loginUser(req,res) {
           email: email
         }
       });
-  
+
       // User is not found
       if (!user) {
         req.session.errors = ['A user with that email address does not exist'];
-  
+
         return res.redirect('/register');
       }
-  
+
       // Validate their password
       const valid_pass = await user.validatePass(password);
-  
+
       if (!valid_pass) {
         req.session.errors = ['Password is invalid'];
         return res.redirect('/login');
       }
-  
+
       req.session.user_id = user.id;
-  
+
       res.redirect('/');
     } catch (err) {
       let messages;
@@ -57,23 +57,23 @@ async loginUser(req,res) {
       messages = [err.message];
 
       console.log(err);
-  
+
       req.session.errors = messages;
-  
+
       res.redirect('/login');
     }
-  
-},
 
-async createRecipe(req, res) {
+  },
+
+  async createRecipe(req, res) {
     try {
-        await Recipe.create(req.body);
+      await Recipe.create(req.body);
 
-        res.redirect('/?recipe_added=true');
-    } catch (error) {
-        console.log(err);
-        res.redirect('/create/recipe');
+      res.redirect('/?recipe_added=true');
+    } catch (err) {
+      console.log(err);
+      res.redirect('/create/recipe');
     }
-}
+  }
 
 }
